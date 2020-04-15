fcd1b141bd0e7204cff4c0106f677b381abaf6ac
Francisco Vaz <ftomasvaz@gmail.com>
2020-04-15 22:46:21 +0100

    Post and get to transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (type !== 'income') {
+      if (type !== 'outcome') {
+        throw Error(`Type must be 'income' or 'outcome'`);
+      }
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.income - balance.outcome < value) {
+      throw Error('Não tem saldo suficiente.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
